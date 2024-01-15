17a55c5f3b1126c29aff86f8cf19531d7004830d
sadreTI <sadretdinov.2004@mail.ru>
2024-01-15 19:00:55 +0500

    06-8 edited

--- 06-08.py
+++ 06-08.py
@@ -1,9 +1,14 @@
 count_mans = int(input('Количество мужчин: '))
 mans_set = set()
+duplicate = set()
 intersection_count = 0
 for i in range(count_mans):
     surname = input('Фамилия: ')
-    if surname in mans_set:
-        intersection_count += 2
-    mans_set.add(surname)
-print(intersection_count)
+    if surname not in mans_set:
+        mans_set.add(surname)
+    else:
+        intersection_count += 1
+        if surname not in duplicate:
+            duplicate.add(surname)
+intersection_count += len(duplicate)
+print(intersection_count, duplicate)

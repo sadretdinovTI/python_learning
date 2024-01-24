1d365ffe896816e9f3cd64d7f5532269f3ad663d
sadreTI <sadretdinov.2004@mail.ru>
2024-01-24 23:07:09 +0500

    12-4 added

---
+++ 12/12-04.py
@@ -0,0 +1,6 @@
+nums = [int(num) for num in input('Enter nums: ').split()]
+start_end = [int(num) for num in input('Start and end: ').split()]
+for num in start_end:
+    if num < 0 or num > len(nums):
+        print('Error')
+print(sum(num**2 for num in nums[start_end[0]:start_end[1] + 1:]))

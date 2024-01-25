0dcdc5b5802f54bad9934fa22cacd58775373857
sadreTI <sadretdinov.2004@mail.ru>
2024-01-25 14:35:26 +0500

    12-8 edited

--- 12/12-08.py
+++ 12/12-08.py
@@ -1,8 +1,10 @@
 line = input("Enter: ").lower()
+print(line)
 letter = 0
 count = 0
 for char in range(len(line)):
-    if line.count(line[char]) > count:
-        count = line.count(line[char])
-        letter = line[char]
-print(letter)
+    if line[char].isalpha():
+        if (line.count(line[char]) > count):
+            count = line.count(line[char])
+            letter = line[char]
+print(letter, count)
